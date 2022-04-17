236cbacc5dd2b796ba77dd7d30b14536dbdd42d6
Krisha Sharma <krvsharm@ucsc.edu>
2022-04-17 01:44:02 +0000

    Graph.h

---
+++ pa2/Graph.h
@@ -0,0 +1,46 @@
+//------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------
+// Krisha Sharma
+// Cruz ID: krvsharm
+// CSE 101-01 Tantalo
+// Programming Assignment 2
+// Graph.h
+//------------------------------------------------------------------------------------------------------
+// Credit: Please Note, there was high level pusedocode collabration with my sister tvsharma 
+// (Twisha Sharma) during this assignment. The below code is also based off of Professor Tantalo's 
+// example code and pusedo-code provided on the class webpage.
+//------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "List.h"
+
+#define NIL = -3;
+#define INF = -100000;
+
+#define WHITE = -30 // as of yet undiscovered 
+#define BLACK = -420 // finished
+#define GRAY = -100 // vertecies discovered, but neighbors not 
+
+/*** Constructors-Destructors ***/
+Graph newGraph(int n);
+void freeGraph(Graph* pG);
+
+/*** Access functions ***/
+int getOrder(Graph G);
+int getSize(Graph G);
+int getSource(Graph G);
+int getParent(Graph G, int u);
+int getDist(Graph G, int u);
+void getPath(List L, Graph G, int u);
+
+/*** Manipulation procedures ***/
+void makeNull(Graph G);
+void addEdge(Graph G, int u, int v);
+void addArc(Graph G, int u, int v);
+void BFS(Graph G, int s);
+
+/*** Other operations ***/
+void printGraph(FILE* out, Graph G);
